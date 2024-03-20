5e1c6b4bd2dbfffea7433dc37c72207867f3444f
Your Name <you@example.com>
2024-03-20 03:05:40 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -143,7 +143,7 @@ int parentesisBalanceados(char *cadena)
 
     if (cadena[i] == ')' || cadena[i] == ']' || cadena[i] == '}')
     {
-      if (top(pila) != NULL)
+      if (pila != NULL)
       {
         if (parentesis == top(pila))
           pop(pila);
